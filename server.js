0cc30e39368842ce50e6c8a6b8c78f881036c7e6
jjacque1 <jacksonjacque.jj@gmail.com>
2026-02-24 02:57:40 -0500

    post route created

--- server.js
+++ server.js
@@ -1,22 +1,53 @@
-const PORT = 5000; //defines the port Node server will listen on for incomming http request
+const PORT = 5000;
+const express = require("express");
 
-const express = require("express"); //import the express framework so we can create and configure a web server
+const app = express();
 
-const app = express(); //creates an express application instance which represents our server
+const cors = require("cors");
 
-const cors = require("cors"); //imports CORS middleware to allow cross-origin requests between different ports (frontend and backend)
+app.use(cors());
+app.use(express.json());
 
-app.use(cors()); //enable the cors resource that allows requests from other origins
-app.use(express.json()); //express built in middleware that parses incoming JSON request bodies and makes them available on req.body
-
-let tasks = []
+let tasks = [];
 
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
-}); //health check endpoint to confirm the server is running and responding to request
+});
 
-app.get("/api/tasks")
+app.get("/api/tasks", (req, res) => {
+    res.json(tasks)
+});
+
+app.post("/api/tasks", (req, res) => {
+
+    if(!req.body){
+        return req.status(400).json({message: "Invalid or Missing JSON body"})
+    }
+    const { title, description, status} = req.body;
+
+    const trimmedTitle = (title || "").trim();
+    const trimmedDescription = (description || "").trim();
+
+    if(!trimmedTitle || !trimmedDescription) {
+        return res.status(400).json({message: "title and description are required"})
+    }
+
+    const allowedStatuses = ["pending", "in-progress", "done"];
+    const finalStatus = allowedStatuses.includes(status) ? status : "pending";
+
+    const newTask = {
+        id: String(Date.now()),
+        title: trimmedTitle,
+        description: trimmedDescription, 
+        status: finalStatus,
+    };
+
+    tasks.push(newTask)
+
+    res.status(201).json(newTask)
+
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-}); //start the server and begins listening for incoming requests on the specified port
+});
